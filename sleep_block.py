6d8d8b6bef3d65f018a3386c095cdafedf9673d3
Chris Brackert <cbrackert@gmail.com>
2015-03-17 17:29:48 -0700

    Add persistence

--- sleep_block.py
+++ sleep_block.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+from time import time as _time
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
 from nio.metadata.properties import TimeDeltaProperty
 from nio.modules.scheduler import Job
+from nio.modules.threading import Lock
 
 
 @Discoverable(DiscoverableType.block)
@@ -18,6 +21,48 @@ class Sleep(Block):
 
     interval = TimeDeltaProperty(title='Interval', default={'seconds': 1})
 
+    def __init__(self):
+        super().__init__()
+        self._signals = []
+        self._signals_lock = Lock()
+
+    def configure(self, context):
+        super().configure(context)
+        self._load_signals = self.persistence.load('signals') or []
+
+    def start(self):
+        super().start()
+        self._schedule_persistence_sleeps()
+
+    def stop(self):
+        super().stop()
+        self._store_signals(_time())
+        self.persistence.save()
+
     def process_signals(self, signals):
         # After interval, notify these signals
         emit = Job(self.notify_signals, self.interval, False, signals)
+        self._store_signals(_time() + self.interval.total_seconds(), signals)
+
+    def _schedule_persistence_sleeps(self):
+        """ Schedule emit jobs for signals loaded from persistence """
+        ctime = _time()
+        for (time, signals) in self._load_signals:
+            # get time to go before the signals should be notified
+            dtime = time - ctime
+            if dtime > 0 and signals:
+                dtime = timedelta(seconds=dtime)
+                Job(self.notify_signals, dtime, False, signals)
+                self._store_signals(time, signals)
+
+    def _store_signals(self, notify_time, signals=None):
+        """ Add new signals and trim old ones before storing """
+        with self._signals_lock:
+            if signals:
+                self._signals.append((notify_time, signals))
+            self._signals = self._trim_old_signals(_time(), self._signals)
+            self.persistence.store('signals', self._signals)
+
+    def _trim_old_signals(self, ctime, signals):
+        """ Remove signals from `signals` older than `ctime` """
+        return [(t, s) for (t, s) in self._signals if ctime <= t]
--- tests/test_sleep_block.py
+++ tests/test_sleep_block.py
@@ -1,8 +1,10 @@
 import time
-from ..sleep_block import Sleep
+from unittest.mock import MagicMock, patch
 from nio.util.support.block_test_case import NIOBlockTestCase
+from nio.modules.persistence.default import Persistence
 from nio.modules.threading import Event
 from nio.common.signal.base import Signal
+from ..sleep_block import Sleep
 
 
 class SleepEvent(Sleep):
@@ -18,12 +20,18 @@ class SleepEvent(Sleep):
 
 class TestSleep(NIOBlockTestCase):
 
+    def get_test_modules(self):
+        return self.ServiceDefaultModules + ['persistence']
+
+    def get_module_config_persistence(self):
+        return {'persistence': 'default'}
+
     def test_sleep_block(self):
         e = Event()
         blk = SleepEvent(e)
         self.configure_block(blk, {
             'log_level': 'DEBUG',
-            'interval': {'seconds': 1}
+            'interval': {'seconds': 0.1}
         })
         blk.start()
         signals = [Signal({'name': 'signal1'}),
@@ -34,5 +42,82 @@ class TestSleep(NIOBlockTestCase):
         e.wait()
         end = time.time()
         self.assert_num_signals_notified(2)
-        self.assertTrue(1.0 < end - start < 1.1)
+        self.assertTrue(0.1 < end - start < 0.5)
         blk.stop()
+
+    def test_persist_save(self):
+        blk = Sleep()
+        self.configure_block(blk, {
+            'log_level': 'DEBUG',
+            'interval': {'seconds': 10}
+        })
+        blk.persistence.store = MagicMock()
+        blk.persistence.save = MagicMock()
+        blk.start()
+        signals_a = [Signal({'name': 'signal1'}),
+                     Signal({'name': 'signal2'})]
+        signals_b = [Signal({'name': 'signal3'}),
+                     Signal({'name': 'signal4'})]
+
+        def assert_persistence(store_count, save_count):
+            self.assertEqual(blk.persistence.store.call_count, store_count)
+            self.assertEqual(blk.persistence.save.call_count, save_count)
+            self.assertEqual(blk.persistence.store.call_args[0][0],
+                            'signals')
+            self.assertEqual(blk.persistence.store.call_args[0][1],
+                            blk._signals)
+
+        # process some signals
+        blk.process_signals(signals_a)
+        assert_persistence(1, 0)
+        # process some more signals
+        blk.process_signals(signals_b)
+        assert_persistence(2, 0)
+        # stop the block and save persistence
+        blk.stop()
+        assert_persistence(3, 1)
+
+        # the block should have stopped before any signals were notified
+        self.assert_num_signals_notified(0)
+        # assert that two groups of signals were saved
+        self.assertEqual(len(blk._signals), 2)
+        # assert that each group has two signals
+        self.assertEqual(len(blk._signals[0][1]), 2)
+        self.assertEqual(len(blk._signals[1][1]), 2)
+        # assert that the second set of signals has a later time than the first
+        self.assertTrue(blk._signals[0][0] < blk._signals[1][0])
+
+    @patch(Sleep.__module__ + '._time')
+    def test_persist_load(self, mock_time):
+        e = Event()
+        blk = SleepEvent(e)
+        signals1 = [Signal()]
+        signals2 = [Signal(), Signal()]
+        signals = [(0, signals1), (1, signals2)]
+        with patch('nio.modules.persistence.default.Persistence.load') as load:
+            load.return_value = signals
+            self.configure_block(blk, {
+                'log_level': 'DEBUG',
+                'interval': {'seconds': 1}
+            })
+        mock_time.return_value = 0.9
+        blk.start()
+        # check that all signals were loaded but only two signals were put
+        # pack into _signals
+        self.assertEqual(blk._load_signals, signals)
+        self.assertEqual(len(blk._signals), 1)
+        self.assertEqual(len(blk._signals[0][1]), 2)
+        self.assert_num_signals_notified(0)
+        e.wait()
+        self.assert_num_signals_notified(2)
+
+    def test_persist_load_nothing(self):
+        e = Event()
+        blk = SleepEvent(e)
+        with patch('nio.modules.persistence.Persistence.load'):
+            self.configure_block(blk, {
+                'log_level': 'DEBUG',
+                'interval': {'seconds': 10}
+            })
+        blk.start()
+        self.assertEqual(blk._load_signals, [])
